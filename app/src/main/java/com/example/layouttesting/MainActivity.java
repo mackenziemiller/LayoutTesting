189525b18be3015b493fbefd5c8b7cd541c74df2
mackenziemiller <78806991+mackenziemiller@users.noreply.github.com>
2021-03-19 12:50:27 -0400

    Layout trials
    Updates after Sam

--- app/src/main/java/com/example/layouttesting/MainActivity.java
+++ app/src/main/java/com/example/layouttesting/MainActivity.java
@@ -4,11 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class MainActivity extends AppCompatActivity {
+    //private LineGraphSeries<DataPoint> series1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
+        double x, y;
+        x = 0;
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+         series1 = new LineGraphSeries<>();
+        int numDataPoint = 500; //this is the number of data points we are plotting
+        for (int i = 0; i < numDataPoint; i++) {
+        x = x + 0.1;//0.1 gives the graph a nice continuous look
+        y = Math.sin(x);
+         series1.appendData(new DataPoint(x, y), true, 100);
+        */
+
     }
-}
+        //graph.addSeries(series1);
+
+    }
--- app/src/main/java/com/example/layouttesting/PressureFragment.java
+++ app/src/main/java/com/example/layouttesting/PressureFragment.java
@@ -1,5 +1,9 @@
 package com.example.layouttesting;
+import androidx.appcompat.app.AppCompatActivity;
 
+
+
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +11,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +34,9 @@ public class PressureFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+
     public PressureFragment() {
+
         // Required empty public constructor
     }
 
@@ -49,6 +61,7 @@ public class PressureFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -58,7 +71,27 @@ public class PressureFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+
+        //View view = inflater.inflate(R.layout.fragment_pressure, container, false);
+        GraphView graph = (GraphView)getActivity().findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series1 = new LineGraphSeries<>();
+        double x,y;
+        x =0;
+
+        int numDataPoint = 500; //this is the number of data points we are plotting
+        for(int i =0; i < numDataPoint ; i++) {
+            //0.1 gives the graph a nice continuous look
+            x = x + 0.1;
+            y = Math.sin(x);
+            series1.appendData(new DataPoint(x, y), true, 100);
+        }
+        graph.addSeries(series1);
         // Inflate the layout for this fragment
+        //FrameLayout layout = view.findViewById(R.id.graph);
         return inflater.inflate(R.layout.fragment_pressure, container, false);
     }
 }
